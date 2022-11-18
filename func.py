2c0d78e3a58afba6ca8d81a8038ba02367396c7d
ferhatozcelikphysis <ferhatozcelikphysis@physis.com>
2022-11-18 23:21:22 +0300

    First Commit

--- func.py
+++ func.py
@@ -1,19 +1,37 @@
 import numpy as np
+
+
 def sq(x, a, b, c, d, e, f, g, h, i, j, k):
-    return a * x + b*x**2 + c
+    return a * x + b * x ** 2 + c
+
+
 def tr(x, a, b, c, d, e, f, g, h, i, j, k):
-    return a * x + b*x**2 + c*x**3 + d
+    return a * x + b * x ** 2 + c * x ** 3 + d
+
+
 def fo(x, a, b, c, d, e, f, g, h, i, j, k):
-    return a * x + b*x**2 + c*x**3 + d*x**4 + e
+    return a * x + b * x ** 2 + c * x ** 3 + d * x ** 4 + e
+
+
 def fi(x, a, b, c, d, e, f, g, h, i, j, k):
-    return a * x + b*x**2 + c*x**3 + d*x**4 + e*x**5 + f
+    return a * x + b * x ** 2 + c * x ** 3 + d * x ** 4 + e * x ** 5 + f
+
+
 def si(x, a, b, c, d, e, f, g, h, i, j, k):
-    return a * x + b*x**2 + c*x**3 + d*x**4 + e*x**5 + f*x**6 + g
+    return a * x + b * x ** 2 + c * x ** 3 + d * x ** 4 + e * x ** 5 + f * x ** 6 + g
+
+
 def se(x, a, b, c, d, e, f, g, h, i, j, k):
-    return a * x + b*x**2 + c*x**3 + d*x**4 + e*x**5 + f*x**6 + g*x**7 + h
+    return a * x + b * x ** 2 + c * x ** 3 + d * x ** 4 + e * x ** 5 + f * x ** 6 + g * x ** 7 + h
+
+
 def ei(x, a, b, c, d, e, f, g, h, i, j, k):
-    return a * x + b*x**2 + c*x**3 + d*x**4 + e*x**5 + f*x**6 + g*x**7 + h*x**8 + i
+    return a * x + b * x ** 2 + c * x ** 3 + d * x ** 4 + e * x ** 5 + f * x ** 6 + g * x ** 7 + h * x ** 8 + i
+
+
 def ni(x, a, b, c, d, e, f, g, h, i, j, k):
-    return a * x + b*x**2 + c*x**3 + d*x**4 + e*x**5 + f*x**6 + g*x**7 + h*x**8 + i*x**9 + j
+    return a * x + b * x ** 2 + c * x ** 3 + d * x ** 4 + e * x ** 5 + f * x ** 6 + g * x ** 7 + h * x ** 8 + i * x ** 9 + j
+
+
 def te(x, a, b, c, d, e, f, g, h, i, j, k):
-    return a * x + b*x**2 + c*x**3 + d*x**4 + e*x**5 + f*x**6 + g*x**7 + h*x**8 + i*x**9 + j*x**10 + k
+    return a * x + b * x ** 2 + c * x ** 3 + d * x ** 4 + e * x ** 5 + f * x ** 6 + g * x ** 7 + h * x ** 8 + i * x ** 9 + j * x ** 10 + k
